4817f94a0267f00cf8c4682dd0facf0259cd93d9
AndresDz2024 <156430122+AndresDz2024@users.noreply.github.com>
2024-01-30 07:26:11 -0500

    Documentación de mi programa

--- Dia3/Ejercicio3_DazaAndres.py
+++ Dia3/Ejercicio3_DazaAndres.py
@@ -2,20 +2,25 @@
 ##-----------------------------------------------
 ##------------- Ejercicio 3 ---------------------
 ##-----------------------------------------------
-
+#Define the function
 def The_number(number):
+    #In case the user enters a negative number.
     if number < 0:
         print("The number entered is negative, please enter a positive number " )
         return
+    #Perform the operation
     coins_10 = number // 10
     number %= 10
     coins_5 = number // 5
     number %= 5
     coins_1 = number
+    #Print the result
     print(f"{coins_10} coins of 10 are used" )
     print(f"{coins_5} coins of 5 are used" )
     print(f"{coins_1} coins of 1 are used" )
+#ask the number
 number = int(input("Write the number: "))
+#call the function
 The_number(number)
 
 ## Programa desarrollado por Andres Santiago Daza Daza --- 1095916023
