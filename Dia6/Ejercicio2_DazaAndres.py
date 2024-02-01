7524d21e495f1ea1cc9ca35154501db1a0c3c274
andresDZ2024 <daza45daza@gmail.com>
2024-02-01 14:18:00 -0500

    inicio del segundo ejercicio

---
+++ Dia6/Ejercicio2_DazaAndres.py
@@ -0,0 +1,26 @@
+##def negate(num):
+    ##return -num
+
+##def large_num(num):
+   ## if /(num > 10000)
+   ## boolean = (num > 10000)
+    
+##negate(b)
+##neg_b = num
+##print 'b:', b, 'neg_b:', neg_b
+
+##big = large_num(b)
+##print 'b is big:', big    
+
+##Bugs
+
+
+##Bug 1
+
+
+##Bug 2
+
+
+##Bug 3
+
+
