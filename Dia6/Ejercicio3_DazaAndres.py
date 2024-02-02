6135248c4f4000142640e3c7fda9e4e72bcfcbb6
andresDZ2024 <daza45daza@gmail.com>
2024-02-01 20:56:22 -0500

    Ejercicio de investigaciòn, tipos de datos mutables e inmutables

---
+++ Dia6/Ejercicio3_DazaAndres.py
@@ -0,0 +1,9 @@
+## String: Secuencia de caracteres textuales
+## Diccionario: Estructura clave - Valor 
+## Tuples: Colección inmutable de datos
+## Listas: Colección mutable de datos
+## Mutable o inmutable 
+## String: inmutable
+## Diccionario: mutable
+## Tuples: inmutables
+## Listas: mutables
