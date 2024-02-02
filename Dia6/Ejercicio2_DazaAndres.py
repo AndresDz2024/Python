dd47aa4ca3c397ab28deb498b04717fdd9552582
andresDZ2024 <daza45daza@gmail.com>
2024-02-01 19:54:26 -0500

    finalizacion ejercicio 2

--- Dia6/Ejercicio2_DazaAndres.py
+++ Dia6/Ejercicio2_DazaAndres.py
@@ -1,26 +1,43 @@
-##def negate(num):
-    ##return -num
+##-------------------------------------------------------------------------------------
+##-------------------------------------------------------------------------------------
+##-------------------------------- Ejercicio 6.1 --------------------------------------
+##-------------------------------------------------------------------------------------
+def negate(num):
+    return -num
 
-##def large_num(num):
-   ## if /(num > 10000)
-   ## boolean = (num > 10000)
+def large_num(num):
+    if (num > 10000):
+        res = True
+    else:
+        res = False 
+    return res    
     
-##negate(b)
-##neg_b = num
-##print 'b:', b, 'neg_b:', neg_b
+num = int(input())
 
-##big = large_num(b)
-##print 'b is big:', big    
+print(negate(num))
+
+print(large_num(num))
 
 ##Bugs
 
-
 ##Bug 1
 
+# The function "large_num"  did not return true or false as requested by the requeriments.
+
+# To solve this bug, i applied an "if" conditional, which takes care of returning the 
+# "True or False" requested in the requirements.
 
 ##Bug 2
 
+# Functions were being called incorrectly.
+
+# To fix this bug, i have used "print" to correctly call the function so that it prints correctly.
 
 ##Bug 3
 
+# As of negate(b) nothing in the base code works according to the requirements.
 
+# To solve this bug, i remove everything unnecesary from the code base and simply requestes the value
+# of "num", so that the code would work correctly, fulfilling all the requirements
+
+## Dessarrollado por Andres Santiago Daza Daza 1095916023
